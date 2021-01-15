de68929922d91a4d08c31646c80856638db2918f
aqaatron <aqaatron@gmail.com>
2021-01-15 15:19:49 +0400

    Ref and Val type

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,11 +13,22 @@ namespace Snake
             //int x1 = 1;
             //int y1 = 3;
             //char sym1 = '*';
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
-            p1.Draw();
+            Point p1 = new Point(3, 1, '*');
+            //p1.x = 1;
+            //p1.y = 3;
+            //p1.sym = '*';
+
+            Point p2 = new Point(4, 5, '_');
+
+
+            p1 = p2;
+
+            p2.x++;
+
+            p2.y++;
+
+            Console.WriteLine($"p1= {p1.x}, {p1.y}");
+
             Console.Read();
         }
         
